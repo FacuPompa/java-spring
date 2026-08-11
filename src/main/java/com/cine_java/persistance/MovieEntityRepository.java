d7a3609ee87f1a2ac7a7b2d1f03ca7a4297ab040
Facundo Pompa <pompafacundo@gmail.com>
2026-08-11 17:57:22 -0300

    mapStruct

---
+++ src/main/java/com/cine_java/persistance/MovieEntityRepository.java
@@ -0,0 +1,26 @@
+package com.cine_java.persistance;
+
+import com.cine_java.domain.dto.MovieDto;
+import com.cine_java.domain.repository.MovieRepository;
+import com.cine_java.persistance.crud.CrudMovieEntity;
+import com.cine_java.persistance.mapper.MovieMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MovieEntityRepository implements MovieRepository {
+    private final CrudMovieEntity crudMovieEntity;
+    private final MovieMapper movieMapper;
+
+    public MovieEntityRepository(CrudMovieEntity crudMovieEntity, MovieMapper movieMapper) {
+        this.crudMovieEntity = crudMovieEntity;
+        this.movieMapper = movieMapper;
+    }
+
+    @Override
+    public List<MovieDto> getAll() {
+
+        return this.movieMapper.toDto(this.crudMovieEntity.findAll());
+    }
+}
---
+++ src/main/java/com/cine_java/persistance/mapper/MovieMapper.java
@@ -0,0 +1,20 @@
+package com.cine_java.persistance.mapper;
+
+import com.cine_java.domain.dto.MovieDto;
+import com.cine_java.persistance.entity.MovieEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface MovieMapper {
+
+    @Mapping(source = "titulo", target = "title")
+    @Mapping(source = "duracion", target = "duration")
+    @Mapping(source = "genero", target = "genre")
+    @Mapping(source = "fechaEstreno", target = "releaseDate")
+    @Mapping(source = "clasificacion", target = "rating")
+    MovieDto toDto(MovieEntity entity);
+    List<MovieDto> toDto(Iterable<MovieEntity> entities);
+}
